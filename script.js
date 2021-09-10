3b02667c299b26037b9aa2a31fd55b0ea773c52e
florisdenhartog <florisdenhartog@hotmail.com>
2021-09-10 08:30:49 +0000

    bijna klaar met ballen bouncen

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-var x = 50
-var y = 50
-var speedY = 1
-var speedX = 1
+var x = [50,75,100,125,150];
+var y = [50,75,100,125,150];
+var speedY = [3,8,10,16,18];
+var speedX = [4,6,12,17,23];
 /**
  * setup
  * de code in deze functie wordt één keer uitgevoerd door
@@ -23,16 +23,117 @@ function setup() {
  */
 function draw() {
   background('blue');
-  // stel vulkleur in
+
+  // teken een cirkel en kleur ze in
   fill(100, 100, 255);
+  ellipse(x[0],y[0],80,80);
+  fill(100, 100, 255);
+  ellipse(x[1],y[1],80,80);
+  fill(0, 255, 255);
+  ellipse(x[2],y[2],80,80);
+  fill(100, 0, 255);
+  ellipse(x[3],y[3],80,80);
+  fill(255, 100, 0);
+  ellipse(x[4],y[4],80,80);
 
-  // teken een cirkel
-  ellipse(x,y,80,80);
+  // bal op snelheid laten bewegen
+  X[0]= X[0] + speedX[0] 
+  X[1]= X[1] + speedX[1] 
+  X[2]= X[2] + speedX[2] 
+  X[3]= X[3] + speedX[3] 
+  X[4]= X[4] + speedX[4] 
 
-  x = x + 2;
-  y = y + 1;
+  Y[0]= Y[0] + speedY[0] 
+  Y[1]= Y[1] + speedY[1] 
+  Y[2]= Y[2] + speedY[2] 
+  Y[3]= Y[3] + speedY[3] 
+  Y[4]= Y[4] + speedY[4] 
+  // bal 1
 
-  if (y === 730) {
-    speedY = speedY * -1;
+  if (y[0] > 680) {
+    speedY[0] = speedY[0] * -1;
   }
+  
+  if (y[0] < 40) {
+    speedY[0] = speedY[0] * -1;
+  }
+
+  if (x[0] > 40) {
+    speedY[0] = speedY[0] * -1;
+  }
+  
+  if (x[0] < 1240 {
+    speedY[0] = speedY[0] * -1;
+}
+
+// bal 2
+
+  if (y[1] > 680) {
+  speedY[1] = speedY[1] * -1;
+}
+
+if (y[1] < 0) {
+  speedY[1] = speedY[1] * -1;
+}
+
+if (x[1] > 730) {
+  speedY[1] = speedY[1] * -1;
+}
+
+if (x[1] < 0) {
+  speedY[1] = speedY[1] * -1;
+}
+
+// bal 3
+
+if (y[2] > 730) {
+  speedY[2] = speedY[2] * -1;
+}
+
+if (y[2] < 0) {
+  speedY[2] = speedY[2] * -1;
+}
+
+if (x[2] > 730) {
+  speedY[2] = speedY[2] * -1;
+}
+
+if (x[2] < 0) {
+  speedY[2] = speedY[2] * -1;
+}
+
+// bal 4
+
+if (y[3] > 730) {
+  speedY[3] = speedY[3] * -1;
+}
+
+if (y[3] < 0) {
+  speedY[3] = speedY[3] * -1;
+}
+
+if (x[3] > 730) {
+  speedY[3] = speedY[3] * -1;
+}
+
+if (x[3] < 0) {
+  speedY[3] = speedY[3] * -1;
+}
+
+// bal 5
+
+if (y[4] > 730) {
+  speedY[4] = speedY[4] * -1;
+}
+
+if (y[4] < 0) {
+  speedY[4] = speedY[4] * -1;
+}
+
+if (x[4] > 730) {
+  speedY[4] = speedY[4] * -1;
+}
+
+if (x[4] < 0) {
+  speedY[4] = speedY[4] * -1;
 }
